c4587be1ce9104c29746424c370e2bacceace993
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-05-31 16:02:37 -0400

    Add real tajimas D

--- analysis/Statistics.py
+++ analysis/Statistics.py
@@ -66,11 +66,21 @@ def centre_of_mass(sample):
 def harmonic_number(n):
 	"""
 		Returns the nth-harmonic number according to:
-		sum_{i=1, n-1} 1/i
+		sum_{i=1, @n} 1/i
 		implemented as https://en.wikipedia.org/wiki/Harmonic_number#Calculation
 
 	"""
+	assert n>0, 'n must be bigger than 0'
 	return GAMMA + np.log(n) + 0.5/n - 1./(12*n**2) + 1./(120*n**4)
+
+def generalized_harmonic_number(n, power):
+	"""
+		Returns the nth-generalized 2nd order harmonic number according to:
+		sum_{i=1, @n} 1/i^@power
+		@TODO: is there a better way to do this?
+	"""
+	assert n>0, 'n must be bigger than 0'
+	return np.sum(1./(np.arange(1,n+1)**power))
 
 def normalized_segregating_sites(SNP_counts, sample_size):
 	"""
@@ -80,3 +90,28 @@ def normalized_segregating_sites(SNP_counts, sample_size):
 			sample_size: the number of samples
 	"""
 	return number_of_segregating_sites(SNP_counts) / float(harmonic_number(sample_size-1))
+
+def tajimas_D(SNP_counts, n, return_parts=False):
+	"""
+		Calculates Tajimas D according to reference below.
+		@params:
+			SNP_counts : SNP counts
+			n : size of the sample
+			return_parts: returns intermediate calculations (S, S/H and pi)
+		Simonsen, K. L., Churchill, G. A., & Aquadro, C. F. (1995). 
+		Properties of statistical tests of neutrality for DNA polymorphism data. 
+		Genetics, 141(1), 413-429.
+	"""
+	# the harmonic numbers
+	an = harmonic_number(n-1)
+	bn = generalized_harmonic_number(n-1, 2)
+	
+	vT = (float(2*(n**2 + n + 3))/(9*n*(n-1)) - float(n+2)/(n*an) + bn/an)/(an**2 + bn)
+	uT = float(float(n+1)/(3*(n-1)) - 1./an)/an - vT
+
+	S = number_of_segregating_sites(SNP_counts)
+	pi = proportion_of_pairwise_differences(SNP_counts, n)
+
+	D = float(pi - float(S)/an)/np.sqrt(uT*S + vT*(S**2))
+	return (pi, S, float(S)/an, D) if return_parts else D
+
