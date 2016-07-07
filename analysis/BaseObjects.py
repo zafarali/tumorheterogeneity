8f367319583714e869259c086e1be7e619480006
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-07-07 17:27:53 -0400

    Add CTC object

--- analysis/BaseObjects.py
+++ analysis/BaseObjects.py
@@ -101,3 +101,59 @@ class Tumor(object):
 
 		return Tumor( Cell.load_cells( cell_file ), \
 					 Genotype.load_genotypes( genome_file ) )
+
+
+class CTC(Tumor):
+	@staticmethod
+	def prepare_CTC(sample, genomes):
+		
+		cell_positions = sample[:,:3]
+		genome_ids = sample[:,3]
+		tm = Tumor(sample, genomes)
+		# need to calculate the COM of the sample and correct for that
+		# COM = centre_of_mass(cell_positions)    
+		
+		cell_positions_corrected = cell_positions-COM
+
+		tm.cells[:,:3] = cell_positions_corrected
+
+		genome_counts = Counter(genome_ids.astype(int))
+		# reprocess the way genomes are stored for reseeding
+		genome_dict = {}
+		for gid, genome in zip(genome_ids, genomes):
+			genome_dict[int(gid)] = genome
+
+		new_mapping = { int(gid):new_id for new_id, gid in enumerate(genome_dict.keys()) }
+		
+		genomes_modified = [0]*len(new_mapping.keys())
+		for original_id, new_id in new_mapping.items():
+
+			original_genome = genomes[original_id]
+			new_frequency = genome_counts[original_id]
+			original_genome.frequency = new_frequency
+			genomes_modified[new_id] = original_genome
+
+		tm.genotypes = genomes_modified
+
+	def cells_to_save(self):
+		# cell positions and genome ids
+		to_return = []
+		for gid, cell in zip(genome_ids,cell_positions_corrected):
+			x,y,z = cell
+			to_return.append([x,y,z, new_mapping[int(gid)]])
+
+		return to_return
+
+	def genomes_to_string(self):
+
+		to_return = []
+		for gid,genome in genome_dict.items():
+			string_builder = str(new_mapping[int(gid)])+' '+str(genome_counts[int(gid)])+' '+str(genome.n_resistant)+' '+str(genome.n_driver)+' '
+			for snp in genome.snps:
+				string_builder+=str(snp)+','
+			string_builder+='-1'
+			
+			to_return.append(string_builder)
+
+		return to_return
+		  
