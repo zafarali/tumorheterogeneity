c01528203fd37902a5baf1f138019a38357518de
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-07-12 16:24:16 -0400

    add flexibility to choose seed

--- model/run.sh
+++ model/run.sh
@@ -1,3 +1,3 @@
-./${1}.exe ${1}_outs 1 1
-cp specs.json ${1}_outs/specs.json
-../pipeline ${1}_outs
+./${1}.exe ${1}_outs_${2} 1 ${2}
+cp specs.json ${1}_outs_${2}/specs.json
+../pipeline ${1}_outs_${2}
