c320dcbf791370974ec213510d0b86342cd818d2
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-07-14 09:54:18 -0400

    Add post-processor for reseeded files, and to select seeds

---
+++ freqspec.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import glob
+import sys
+
+folder = sys.argv[1]
+bins = np.linspace(0,0.5,num=500)
+to_be_meaned = []
+for path in glob.glob(folder+'*all_PMs*'):
+    data = pd.read_csv(path, sep=' ', names=['Num', 'SNP', 'abundancy'])
+    y,_ = np.histogram(data.abundancy, bins=bins)
+    to_be_meaned.append(y)
+
+np.save(folder+'PMfreqs_averaged.npy',np.mean(to_be_meaned, axis=0))
+
+np.save(folder+'bins.npy',bins)
+
+
+to_be_meaned = []
+for path in glob.glob(folder+'*drv_PMs*'):
+    data = pd.read_csv(path, sep=' ', names=['Num', 'SNP', 'abundancy'])
+    y,_ = np.histogram(data.abundancy, bins=bins)
+    to_be_meaned.append(y)
+
+np.save(folder+'drvfreqs_averaged.npy',np.mean(to_be_meaned, axis=0))
---
+++ model/msub_creator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import glob
+import pandas as pd
+import numpy as np
+import random
+import os
+import stat
+import sys
+
+folder = sys.argv[1]
+model = folder[:5] if len(sys.argv) < 2 else sys.argv[2]
+if model[:2] == './':
+    raise Exception("Make sure to include absolute path only...")
+    
+bins = [(2,7), (8,12), (13,17), (18,23), (23,30)]
+all_samples = glob.glob('./'+folder+'/pipe*/samples/*')
+print all_samples
+
+all_data = []
+for pathname in all_samples:
+    details = pathname.split('/')[-1]
+    detail_split =  details.split('size')[-1].split('dist')
+    sz = int(detail_split[0])
+    detail_split = detail_split[-1].split('haplo')
+    dist = float(detail_split[0])
+    nhaplo = int(detail_split[-1].split('i')[0])
+    all_data.append([sz,dist,nhaplo,pathname])
+    
+all_data = pd.DataFrame(all_data, columns=['samplesize', 'distance', 'nhaplotypes', 'pathname'])
+all_data = all_data[all_data['samplesize'] < 30]
+
+
+
+commands = []
+for binnum,limit in enumerate(bins):
+    subsample = all_data[(all_data['samplesize'] <= limit[1]) & (all_data['samplesize'] >= limit[0])]
+    selected = random.sample(subsample.values, 3)
+    local_commands = []
+    for select in selected:
+        cells_file = glob.glob(select[3]+'/cells*')[0]
+        gen_file = glob.glob(select[3]+'/gen*')[0]
+        det_file = glob.glob(select[3]+'/det*')[0]
+        # local_commands.append('cp '+cells_file+' ./')
+        # local_commands.append('cp '+gen_file+' ./')
+        local_commands.append('./'+model+'_reseed.exe reseeded_'+model+'-'+str(binnum)+' 1 1 '+cells_file+' '+gen_file+' \n')
+        local_commands.append('cp '+det_file+' ./reseeded_'+model+'-'+str(binnum)+'/ \n')
+    local_commands.append('../freqspec.py '+model+'_reseeded_'+model+'-'+str(binnum)+'/ \n')
+    commands.append(local_commands)
+
+    
+for i,commands in enumerate(commands):
+    filename = './'+model+'_executable'+str(i)+'.sh'
+    with open(filename, 'w') as f:
+        f.writelines(commands)
+    st = os.stat(filename)
+    os.chmod(filename, st.st_mode | stat.S_IEXEC)
