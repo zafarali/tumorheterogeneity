64c5ba4ffad734b9bdfde75fcc47ceb37846e878
Zafarali Ahmed <Zafarali.ahmed@gmail.com>
2017-09-12 10:33:49 -0400

    Modify script to run on abacus

--- model/run.sh
+++ model/run.sh
@@ -1,3 +1,7 @@
+export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
+module use ${MUGQIC_INSTALL_HOME}/modulefiles
+module load mugqic/python/2.7.11
+
 ./${1}.exe ${1}_outs_${2} 1 ${2} ${@:3}
 cp specs.json ${1}_outs_${2}/specs.json
-../pipeline ${1}_outs_${2}
+python2.7 ../pipeline ${1}_outs_${2}
