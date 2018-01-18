351f5e968c15f932b05795752bec1684956ca14b
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-01-18 11:01:52 -0500

    Add cluster launchers

---
+++ model/pipeline_for_one.sh
@@ -0,0 +1,7 @@
+# in run analysis
+export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
+module use ${MUGQIC_INSTALL_HOME}/modulefiles
+module load mugqic/python/2.7.11
+
+python2.7 ../post_pipeline.py ${1}
+
