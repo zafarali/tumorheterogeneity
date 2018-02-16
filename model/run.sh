66b4eba156dc3d6e8248b6d91da94f9833ef6c2e
Zafarali Ahmed <zafarali.ahmed@mail.mcgill.ca>
2018-02-16 09:21:04 -0500

    remove hard coded mutation rate

--- model/run.sh
+++ model/run.sh
@@ -9,7 +9,7 @@ module load mugqic/python/2.7.11
 ./experiments/${2}/${1}.exe "${1}_outs_${3}_TEMP" 1 ${3}
 cp specs.json ${1}_outs_${3}_TEMP/specs.json
 python2 ../pipeline ${1}_outs_${3}_TEMP
-mkdir ./experiments/u0.01/${1}_outs_${3}
-mv ${1}_outs_${3}_TEMP/* ./experiments/u0.01/${1}_outs_${3}
-cp -r ${1}_outs_${3}_TEMP/*pipe_out* ./experiments/u0.01/${1}_outs_${3}
+mkdir ./experiments/${2}/${1}_outs_${3}
+mv ${1}_outs_${3}_TEMP/* ./experiments/${2}/${1}_outs_${3}
+cp -r ${1}_outs_${3}_TEMP/*pipe_out* ./experiments/${2}/${1}_outs_${3}
 rm -rf ${1}_outs_${3}_TEMP
