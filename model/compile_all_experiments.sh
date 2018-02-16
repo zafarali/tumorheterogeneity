b69824e2ce7d9fb674a6bf40066d84606f3d3de9
zafarali <zafarali.ahmed@gmail.com>
2018-02-15 22:54:03 -0500

    Add scripts to compile all the experiments into executables

---
+++ model/compile_all_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir experiments
+mkdir experiments/u0.01
+mkdir experiments/u0.01875
+
+for mutation_rate in "u0.01" "u0.01875"; do
+    for filename in $(echo "./parameter_files/$mutation_rate/*.h" | cut -d ' ' -f 1); do
+        echo "compiling $filename";
+        # mutarate=$(echo $filename | cut -d '/' -f 3);
+        # echo $substring;
+        fileroot=$(basename $filename .h);
+        # echo $fileroot;
+        cp $filename ./params.h
+        g++ simulation.cpp main.cpp functions.cpp -w -O3 -o ./$fileroot.exe
+        mv $fileroot.exe "./experiments/$mutation_rate/$fileroot.exe"
+    done
+done
