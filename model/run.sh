353a6f29a7683668f74b710f26aa66250628b04b
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-01 11:38:02 -0500

    Fix moving around of files

--- model/run.sh
+++ model/run.sh
@@ -6,10 +6,10 @@ export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
 module use ${MUGQIC_INSTALL_HOME}/modulefiles
 module load mugqic/python/2.7.11
 
-./experiments/${2}/${1}.exe "${1}_outs_${3}_TEMP" 1 ${3}
-cp specs.json ${1}_outs_${3}_TEMP/specs.json
-python2 ../pipeline ${1}_outs_${3}_TEMP
-mkdir ./experiments/${2}/${1}_outs_${3}
-mv ${1}_outs_${3}_TEMP/* ./experiments/${2}/${1}_outs_${3}
-cp -r ${1}_outs_${3}_TEMP/*pipe_out* ./experiments/${2}/${1}_outs_${3}
-rm -rf ${1}_outs_${3}_TEMP
+./experiments/${2}/${1}.exe "${1}_outs_${3}_${2}_TEMP" 1 ${3}
+cp specs.json ${1}_outs_${3}_${2}_TEMP/specs.json
+python2 ../pipeline ${1}_outs_${3}_${2}_TEMP
+#mkdir ./experiments/${2}/${1}_outs_${3}
+mv ${1}_outs_${3}_${2}_TEMP/ ./experiments/${2}/${1}_outs_${3}
+#cp -r ${1}_outs_${3}_${2}_TEMP/*pipe_out* ./experiments/${2}/${1}_outs_${3}
+#rm -rf ${1}_outs_${3}_${2}_TEMP
