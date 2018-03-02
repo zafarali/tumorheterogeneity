e3ac2bde1d2ef57f993174cf6d10641224e48e82
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-02 15:12:03 -0500

    Working tree computer, need branch lengths

---
+++ analysis/newick_tree_maker.py
@@ -0,0 +1,62 @@
+from collections import Counter
+from newick import loads
+import string
+
+
+def neighbor_joining_tree(genomes):
+    """
+    Runs a version of the neighbour joining tree algorithm
+    :param genomes: a list of genomes
+    """
+    counter = Counter()
+    [counter.update(genome.snps) for genome in genomes]
+    count_order = counter.most_common()
+
+    def split(genomes_L, genomes_R, count_order, split_on):
+        #         print(split_on)
+        snp_id, count = count_order[split_on]
+
+        if len(genomes_L) >= 1 and split_on < len(count_order) - 1:
+            genomes_in_L = []
+            genomes_not_L = []
+
+            for genome in genomes_L:
+                #                 genome.print_tree_form_true()
+                if snp_id in genome.snps:
+                    genomes_in_L.append(genome)
+                else:
+                    genomes_not_L.append(genome)
+            L_processed = split(genomes_in_L, genomes_not_L, count_order, split_on + 1)
+
+        else:
+            L_processed = genomes_L
+
+        if len(genomes_R) >= 1 and split_on < len(count_order) - 1:
+            genomes_in_R = []
+            genomes_not_R = []
+
+            for genome in genomes_R:
+                #                 genome.print_tree_form_true()
+                if snp_id in genome.snps:
+                    genomes_in_R.append(genome)
+                else:
+                    genomes_not_R.append(genome)
+            R_processed = split(genomes_in_R, genomes_not_R, count_order, split_on + 1)
+        else:
+            R_processed = genomes_R
+
+        to_return = []
+
+        if len(L_processed) == 1 and len(R_processed) == 1 and tuple(L_processed[0]) == tuple(R_processed[0]):
+            return tuple(L_processed)
+        if len(L_processed) > 0: to_return += [L_processed] if len(L_processed) > 1 else L_processed
+        if len(R_processed) > 0: to_return += [R_processed] if len(R_processed) > 1 else R_processed
+        return tuple(to_return)
+
+    computed = str(split(genomes, [], count_order, 0)[0])
+    computed = computed.replace(',)', '').replace('\'', '')
+    for genome in genomes:
+        computed = computed.replace(str(genome), str(genome.original_id))
+
+    computed = computed.replace(']', ')').replace('[', '(')
+    return computed
