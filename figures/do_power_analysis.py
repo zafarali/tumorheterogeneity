db0c273fb83dee72bb57fa97513509d527044f9a
zafarali <zafarali.ahmed@gmail.com>
2018-03-04 16:08:32 -0500

    Fix missing trailing slash

--- figures/do_power_analysis.py
+++ figures/do_power_analysis.py
@@ -5,8 +5,8 @@ import statsmodels.api as sm
 import json
 
 
-NOTURNOVER_FOLDER  = '../model/experiments/u0.01875/1_0_0_outs_10/Mar1pipe_out_Fri_Mar__2_20_42_31_2018'
-TURNOVER_FOLDER  = '../model/experiments/u0.01875/1_0_02_outs_10/Mar1pipe_out_Fri_Mar__2_20_42_31_2018'
+NOTURNOVER_FOLDER  = '../model/experiments/u0.01875/1_0_0_outs_10/Mar1pipe_out_Fri_Mar__2_20_42_31_2018/'
+TURNOVER_FOLDER  = '../model/experiments/u0.01875/1_0_02_outs_10/Mar1pipe_out_Fri_Mar__2_20_42_31_2018/'
 
 FOLDER = TURNOVER_FOLDER
 
