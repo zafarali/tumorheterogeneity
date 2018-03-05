8972f969dfe1459b47feb02070cf03d3a33502dc
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-05 15:19:42 -0500

    Fix seed iterator

--- figures/create_trees.py
+++ figures/create_trees.py
@@ -20,28 +20,27 @@ from analysis.newick import loads
 Trees
 """
 
-def create_trees(root_folder, seeds):
-    for seed in seeds:
-        cluster_search = root_folder +'_outs_'+seed+'/Mar*/cluster_data.json'
-        mixing_search = root_folder +'_outs_'+seed+'/Mar*/mixing_analysis.npy'
-        print('looking in:',cluster_search, mixing_search)
-        cluster_data = glob(cluster_search)
-        mixing_data = glob(mixing_search)
-        for cluster_data_,mixing_data_ in zip(cluster_data, mixing_data):
-            data = json.load(open(cluster_data_, 'r'))
-            mixing_data = np.load(mixing_data_)
-            sorted_idx = np.argsort(mixing_data[:, 0, 0])
-            for idx in sorted_idx[:4].tolist() + sorted_idx[-4:].tolist():
-                print(idx)
-                print(data[idx])
-                distance_from_COM = np.sqrt(np.sum(np.array(data[idx]['COM'])**2))
-                tree_string, mapper = neighbor_joining_tree(data[idx]['genotypes'])
-                print('START'*20)
-                print(mapper)
-                print(distance_from_COM)
-                print(tree_string)
-                print(loads(tree_string)[0].ascii_art().encode('utf-8'))
-                print('END'*40)
+def create_trees(root_folder, seed):
+    cluster_search = root_folder +'_outs_'+seed+'/Mar*/cluster_data.json'
+    mixing_search = root_folder +'_outs_'+seed+'/Mar*/mixing_analysis.npy'
+    print('looking in:',cluster_search, mixing_search)
+    cluster_data = glob(cluster_search)
+    mixing_data = glob(mixing_search)
+    for cluster_data_,mixing_data_ in zip(cluster_data, mixing_data):
+        data = json.load(open(cluster_data_, 'r'))
+        mixing_data = np.load(mixing_data_)
+        sorted_idx = np.argsort(mixing_data[:, 0, 0])
+        for idx in sorted_idx[:4].tolist() + sorted_idx[-4:].tolist():
+            print(idx)
+            print(data[idx])
+            distance_from_COM = np.sqrt(np.sum(np.array(data[idx]['COM'])**2))
+            tree_string, mapper = neighbor_joining_tree(data[idx]['genotypes'])
+            print('START'*20)
+            print(mapper)
+            print(distance_from_COM)
+            print(tree_string)
+            print(loads(tree_string)[0].ascii_art().encode('utf-8'))
+            print('END'*40)
 
 PATH, SEED = sys.argv[1], sys.argv[2]
 create_trees(PATH, SEED)
