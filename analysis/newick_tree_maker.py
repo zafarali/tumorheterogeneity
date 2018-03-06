787961a38ee26335981e7a7740fcfe2ec0fe3428
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-06 10:36:37 -0500

    Change tree printer to be newick mode

--- analysis/newick_tree_maker.py
+++ analysis/newick_tree_maker.py
@@ -1,6 +1,4 @@
 from collections import Counter
-import string
-import re
 
 def number_yielder():
     counter = 0
@@ -8,63 +6,140 @@ def number_yielder():
         yield str(counter)
         counter += 1
 
-def neighbor_joining_tree(genomes):
+def split(genomes, by):
     """
-    Runs a version of the neighbour joining tree algorithm
-    :param genomes: a list of genomes
+    splits the list of genomes by the snp "by"
+    :param genomes:
+    :param by:
+    :return:
     """
-    counter = Counter()
-    [counter.update(genome) for genome in genomes]
-    count_order = counter.most_common()
-
-    def split(genomes_L, genomes_R, count_order, split_on):
-        #         print(split_on)
-        snp_id, count = count_order[split_on]
-
-        if len(genomes_L) >= 1 and split_on < len(count_order) - 1:
-            genomes_in_L = []
-            genomes_not_L = []
-
-            for genome in genomes_L:
-                #                 genome.print_tree_form_true()
-                if snp_id in genome:
-                    genomes_in_L.append(genome)
-                else:
-                    genomes_not_L.append(genome)
-            L_processed = split(genomes_in_L, genomes_not_L, count_order, split_on + 1)
-
+    L = []
+    R = []
+    if len(genomes) == 1:
+        return genomes, R
+    for g in genomes:
+        if by in g:
+            L.append(g)
         else:
-            L_processed = genomes_L
+            R.append(g)
+    return L, R
 
-        if len(genomes_R) >= 1 and split_on < len(count_order) - 1:
-            genomes_in_R = []
-            genomes_not_R = []
+def prepare_snp_data(genomes):
+    """
+    Prepares the genomes by removing duplicates
+    :param genomes:
+    :return:
+    """
+    data = set(map(frozenset, [[-1]+g for g in genomes]))
+    all_snps = Counter()
+    [all_snps.update(data_) for data_ in data]
+    return data, all_snps.most_common()
 
-            for genome in genomes_R:
-                #                 genome.print_tree_form_true()
-                if snp_id in genome:
-                    genomes_in_R.append(genome)
-                else:
-                    genomes_not_R.append(genome)
-            R_processed = split(genomes_in_R, genomes_not_R, count_order, split_on + 1)
+"""
+The object will be used to create trees from genomes
+"""
+class Node(object):
+    def __init__(self, genomes, all_snps=None, snp_to_check=-1):
+        self.genomes = genomes
+        self.branch_length = 0
+        self.resolved = False
+        self.snp_to_check = snp_to_check
+        self.all_snps = all_snps
+        self.L = None
+        self.R = None
+        self.descendants = len(genomes)
+
+    #         print('CREATED NEW NODE:', genomes, snp_to_check)
+
+    def resolve(self):
+        if self.resolved:
+            return
+        if self.snp_to_check is None or self.all_snps is None:
+            self.resolved = True
+            return
+
+        L, R = [], []
+        while len(L) == 0 or len(R) == 0:
+            if self.snp_to_check + 1 >= len(self.all_snps):
+                # run out of snps to check
+                self.snp_to_check = None
+                self.resolved = True
+                # do nothing here. just exit
+                return
+            else:
+                # increment the snp we are checking
+                self.snp_to_check += 1
+            # print('checking', self.all_snps[self.snp_to_check][0])
+            # attempt to do a split
+            L, R = split(self.genomes, self.all_snps[self.snp_to_check][0])
+            if len(R) == 0 and len(L) != 0:
+                # everyone is on the same branch
+                # just increase the length of this one
+                if self.all_snps[self.snp_to_check][0] in L[0]:
+                    self.branch_length += 1
+
+        # we exit this loop, this means that
+        # we have an non trivial split.
+        self.resolved = True
+
+        # first deal with the left branch these are the ones that have.
+        # avoid checking the same snp again so self.snp_to_check + 1
+        self.L = Node(L, self.all_snps, self.snp_to_check)
+        # increase the branch length because we know everyone on L
+        # has this snp.
+        self.L.branch_length += 1
+        self.R = Node(R, self.all_snps, self.snp_to_check)
+        self.L.resolve()
+        self.R.resolve()
+
+    def represent_internals(self):
+        if self.L is None and self.R is None:
+            return str(self.genomes)
         else:
-            R_processed = genomes_R
+            return str(self.L) + ',' + str(self.R)
 
-        to_return = []
+    def __str__(self):
+        return self.__repr__()
 
-        if len(L_processed) == 1 and len(R_processed) == 1 and tuple(L_processed[0]) == tuple(R_processed[0]):
-            return tuple(L_processed)
-        if len(L_processed) > 0: to_return += [L_processed] if len(L_processed) > 1 else L_processed
-        if len(R_processed) > 0: to_return += [R_processed] if len(R_processed) > 1 else R_processed
-        return tuple(to_return)
+    def __repr__(self):
+        start = '['
+        end = ']'
+        if self.resolved:
+            start = '('
+            end = ')'
 
-    computed = str(split(genomes, [], count_order, 0)[0])
-    computed = computed.replace(',)', '').replace('\'', '')
-    mapper = dict(zip(map(str, genomes), number_yielder()))
-    for genome in genomes:
-        computed = computed.replace(str(genome), mapper[str(genome)])
+        if self.L is None and self.R is None:
+            start = ''
+            end = ''
 
-    computed = computed.replace(']', ')').replace('[', '(')
-    computed = re.sub('(((\d+,) )+)', lambda g: '', computed)
-    computed = re.sub('(\(\d+\))', lambda g: g.group(0).replace(')', '').replace('(', ''), computed)
-    return computed, mapper
+        return start + self.represent_internals() + end + ':' + str(self.branch_length)
+
+    def assign_names(self, name_mapper):
+        if self.L is None and self.R is None and len(self.genomes) == 1:
+            self.genomes = name_mapper[self.genomes[0]]
+        else:
+            self.L.assign_names(name_mapper)
+            self.R.assign_names(name_mapper)
+
+    def plot(self, width, midpoint, top_start=0, ax=None):
+        """ Plots the lineage and all its descending lineages. To avoid overlap when plotting
+        multiple lineages, we specify the width and center of the lineage along the x-axis.
+        Time is plotted along the y axis, and uses the lineage T0 and T1"""
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+        branch_height = top_start - self.branch_length - 0.1
+        ax.plot([midpoint, midpoint], [top_start, branch_height], 'b')  # Plot vertical lineage
+        if not (self.L is None and self.R is None):
+            # Assign width proportional to the number of lineages in each sub-lineage
+            n1 = self.R.descendants
+            n2 = self.L.descendants
+            width_1 = n1 * 1. / (n1 + n2) * width
+            width_2 = n2 * 1. / (n1 + n2) * width
+            midpoint_1 = midpoint - width / 2. + width_1 / 2.  # Find the midpoint of each window
+            midpoint_2 = midpoint + width / 2. - width_2 / 2.
+            # Plot horizontal connector
+            ax.plot([midpoint_1, midpoint_2], [branch_height, branch_height], 'b')
+            self.L.plot(width_1, midpoint_1, branch_height, ax)  # Plot descending lineages
+            self.R.plot(width_2, midpoint_2, branch_height, ax)
+
--- figures/create_trees.py
+++ figures/create_trees.py
@@ -12,7 +12,7 @@ sns.set_style('white')
 sns.set_context('paper', font_scale=1.5)
 from glob import glob
 from figtools import *
-from analysis.newick_tree_maker import neighbor_joining_tree
+from analysis.newick_tree_maker import prepare_snp_data, Node
 import json
 from analysis.newick import loads
 
@@ -26,23 +26,28 @@ def create_trees(root_folder, seed):
     print('looking in:',cluster_search, mixing_search)
     cluster_data = glob(cluster_search)
     mixing_data = glob(mixing_search)
-    for cluster_data_,mixing_data_ in zip(cluster_data, mixing_data):
+    for z, (cluster_data_,mixing_data_) in enumerate(zip(cluster_data, mixing_data)):
         data = json.load(open(cluster_data_, 'r'))
-        mixing_data = np.load(mixing_data_)
-        sorted_idx = np.argsort(mixing_data[:, 0, 0])
-        for idx in sorted_idx[:4].tolist() + sorted_idx[-4:].tolist():
-            print(idx)
-            print(data[idx])
-            distance_from_COM = np.sqrt(np.sum(np.array(data[idx]['COM'])**2))
-            tree_string, mapper = neighbor_joining_tree(data[idx]['genotypes'])
-            print('START'*20)
-            print(mapper)
-            print(distance_from_COM)
-            print(tree_string)
-            print(loads(tree_string)[0].ascii_art().encode('utf-8'))
-            print('END'*40)
+        mixing_data_ = np.load(mixing_data_)
+        sorted_idx = np.argsort(mixing_data_[:, 0, 0])
+        panel = '24'
+        fig = plt.figure(figsize=(18, 10))
 
-PATH, SEED = sys.argv[1], sys.argv[2]
+        for panel_count, idx in enumerate(sorted_idx[:4].tolist() + sorted_idx[-4:].tolist()):
+            panel_ = panel + str(panel_count+1)
+            ax = fig.add_subplot(panel_)
+            # print(data[idx])
+            distance_from_COM = np.sqrt(np.sum(np.array(data[idx]['COM'])**2))
+            data_, all_snps = prepare_snp_data(data[idx]['genotypes'])
+
+            tree = Node(data_, all_snps=all_snps)
+            tree.resolve()
+            tree.plot(1, 0, ax=ax)
+            ax.set_title('distance:{:3g}'.format(distance_from_COM))
+
+        fig.savefig(root_folder+'_outs_'+seed+'/tree_plots_{}.pdf'.format(z))
+
+# PATH, SEED = sys.argv[1], sys.argv[2]
 create_trees(PATH, SEED)
 # create_trees('../model/no_death', '1')
 
