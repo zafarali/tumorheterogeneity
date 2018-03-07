8c661cfa7e13006200bea8628f6175fec8350a74
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-07 13:51:35 -0500

    RRemove max namamespace clash, add save fig call

--- figures/create_mixing_analysis.py
+++ figures/create_mixing_analysis.py
@@ -101,7 +101,7 @@ def plot_ranked_mixing_analysis(root_folder, label, fig, to_plot, color='r',
             sns.despine()
         # ax.legend()
         for ax in all_axes:
-            ax.set_ylim([0, max])
+            ax.set_ylim([0, max_value])
 
         break
 
@@ -115,4 +115,4 @@ plot_ranked_mixing_analysis(PATH+'01_outs_'+SEED, 'Turnover (d=0.1)', fig, thing
 plot_ranked_mixing_analysis(PATH+'02_outs_'+SEED, 'Turnover (d=0.2)', fig, thing_to_plot, sort=False, total_rows=5, row_to_start=3)
 plot_ranked_mixing_analysis(PATH+'065_outs_'+SEED, 'Turnover (d=0.65)', fig, thing_to_plot, sort=False, total_rows=5, row_to_start=4, show_labels=True)
 fig.tight_layout(h_pad=1)
-
+fig.savefig('./mixing_analys.pdf')
