189b207a37c06b1aed96e974194f687ed5751a8e
zafarali <zafarali.ahmed@gmail.com>
2018-03-11 00:15:05 -0500

    Stop double printing

--- figures/create_trees.py
+++ figures/create_trees.py
@@ -20,14 +20,14 @@ import cPickle as pickle
 Trees
 """
 def create_trees(root_folder, seed, title, fig, total_rows=1, row_to_start=0):
-    cluster_search = root_folder +'_outs_'+seed+'/Mar*/cluster_data.json'
-    mixing_search = root_folder +'_outs_'+seed+'/Mar*/mixing_analysis.npy'
+    cluster_search = root_folder +'_outs_'+seed+'/Mar1*/cluster_data.json'
+    mixing_search = root_folder +'_outs_'+seed+'/Mar1*/mixing_analysis.npy'
     print('looking in:',cluster_search, mixing_search)
     cluster_data = glob(cluster_search)
     mixing_data = glob(mixing_search)
 
 
-
+    print(cluster_data)
     for z, (cluster_data_,mixing_data_) in enumerate(zip(cluster_data, mixing_data)):
         data = json.load(open(cluster_data_, 'r'))
         # mixing_data_ = np.load(mixing_data_)
@@ -38,12 +38,13 @@ def create_trees(root_folder, seed, title, fig, total_rows=1, row_to_start=0):
             fig = plt.figure(figsize=(13, 3))
         all_axes = []
         for panel_count, idx in enumerate(sorted_idx[:4].tolist() + sorted_idx[-4:].tolist()):
+            print('Creating plots from:', cluster_data_)
             ax = fig.add_subplot(total_rows, 8, (8 * row_to_start) + panel_count + 1)
             if panel_count == 0: ax.set_ylabel(title)
             distance_from_COM = np.sqrt(np.sum(np.array(data[idx]['COM'])**2))
             data_, all_snps = prepare_snp_data(data[idx]['genotypes'])
-            if z == 0 and panel_count == 0:
-                pickle.dump(data[idx]['genotypes'], open('DUMPED.pkl', 'wb'))
+            # if z == 0 and panel_count == 0:
+            #     pickle.dump(data[idx]['genotypes'], open('DUMPED.pkl', 'wb'))
             tree = Node(data_, all_snps=all_snps.most_common())
             tree.resolve()
             tree.plot(1, 0, ax=ax)
@@ -70,31 +71,31 @@ def create_trees(root_folder, seed, title, fig, total_rows=1, row_to_start=0):
     return fig
 
 PATH, SEED = sys.argv[1], sys.argv[2]
-# create_trees('../model/no_death', '1')
-# fig = plt.figure(figsize=(14,10))
-# create_trees(PATH+'/1_0_0', SEED, 'No Turnover', fig, total_rows=5, row_to_start=0)
-# create_trees(PATH+'/1_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
-# create_trees(PATH+'/1_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
-# create_trees(PATH+'/1_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
-# create_trees(PATH+'/1_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
-# fig.tight_layout(h_pad=1)
-# fig.savefig('./trees_selection.pdf')
+create_trees('../model/no_death', '1')
+fig = plt.figure(figsize=(14,10))
+create_trees(PATH+'/1_0_0', SEED, 'No Turnover', fig, total_rows=5, row_to_start=0)
+create_trees(PATH+'/1_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
+create_trees(PATH+'/1_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
+create_trees(PATH+'/1_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
+create_trees(PATH+'/1_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
+fig.tight_layout(h_pad=1)
+fig.savefig('./trees_selection.pdf')
 
 fig = plt.figure(figsize=(14,10))
 create_trees(PATH+'/0_0_0', SEED, 'No Turnover', fig, total_rows=5, row_to_start=0)
-# create_trees(PATH+'/0_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
-# create_trees(PATH+'/0_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
-# create_trees(PATH+'/0_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
-# create_trees(PATH+'/0_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
+create_trees(PATH+'/0_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
+create_trees(PATH+'/0_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
+create_trees(PATH+'/0_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
+create_trees(PATH+'/0_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
 fig.tight_layout(h_pad=1)
 fig.savefig('./trees_no_selection.pdf')
 
-# fig = plt.figure(figsize=(14,10))
-# create_trees(PATH+'/10_0_0', SEED, 'No Turnover', fig, total_rows=5, row_to_start=0)
-# create_trees(PATH+'/10_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
-# create_trees(PATH+'/10_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
-# create_trees(PATH+'/10_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
-# create_trees(PATH+'/10_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
-# fig.tight_layout(h_pad=1)
-# fig.savefig('./trees_extreme_selection.pdf')
+fig = plt.figure(figsize=(14,10))
+create_trees(PATH+'/10_0_0', SEED, 'No Turnover', fig, total_rows=5, row_to_start=0)
+create_trees(PATH+'/10_0_005', SEED, 'Turnover (d=0.05)', fig, total_rows=5, row_to_start=1)
+create_trees(PATH+'/10_0_01', SEED, 'Turnover (d=0.1)', fig, total_rows=5, row_to_start=2)
+create_trees(PATH+'/10_0_02', SEED, 'Turnover (d=0.2)', fig, total_rows=5, row_to_start=3)
+create_trees(PATH+'/10_0_065', SEED, 'Turnover (d=0.65)', fig, total_rows=5, row_to_start=4)
+fig.tight_layout(h_pad=1)
+fig.savefig('./trees_extreme_selection.pdf')
 
