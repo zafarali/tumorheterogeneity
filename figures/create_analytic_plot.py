cb8c6e5047dc14c7417ac924dbc56bf718f58722
zafarali <zafarali.ahmed@gmail.com>
2018-03-12 21:59:03 -0400

    Modifications to colorification

--- figures/create_analytic_plot.py
+++ figures/create_analytic_plot.py
@@ -41,9 +41,9 @@ sfs, x_c_prime = SFS(10**8, mu, fusco_alpha, fusco_beta, with_correction=False)
 
 sfs = np.vectorize(sfs)
 fusco_support = np.logspace(-4.1, np.log10(x_c_prime), num=1000, endpoint=False)
-ax.plot(np.log10(fusco_support), np.log10(sfs(2*fusco_support)), '-b', label='Fusco et al. (Without Correction)')
-fusco_support = np.logspace(np.log10(x_c_prime), 0, num=1000, endpoint=False)
-ax.plot(np.log10(fusco_support), np.log10(sfs(2*fusco_support)), '-b')
+ax.plot(np.log10(fusco_support), np.log10(sfs(2*fusco_support)), ':b', label='Fusco et al. (Without Correction)')
+fusco_support = np.logspace(np.log10(x_c_prime)+np.finfo(float).eps, 0, num=1000, endpoint=False)
+ax.plot(np.log10(fusco_support), np.log10(sfs(2*fusco_support)), ':b')
 
 
 fusco_support = np.logspace(-4.1, 0, num=1000)
@@ -52,7 +52,7 @@ sfs = np.vectorize(sfs)
 ax.plot(np.log10(fusco_support), np.log10(sfs(2*fusco_support)), '--r', label='Fusco et al. (With Correction)')
 
 freq_support = np.logspace(-2.05,0, num=1000)
-ax.plot(np.log10(freq_support), np.log10((alpha*mu/(4*np.sqrt(np.pi)))*freq_support**(-2.5)), '--g', label='Deterministic Result')
+ax.plot(np.log10(freq_support), np.log10((alpha*mu/(4*np.sqrt(np.pi)))*freq_support**(-2.5)), '-', label='Deterministic Result')
 
 ax.set_xlabel('$log_{10}(f)$')
 ax.set_ylabel('$log_{10}(p(f))$')
