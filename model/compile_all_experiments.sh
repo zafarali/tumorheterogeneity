855621ac92d0b8d0a12a80a9c5a5a0cf5ec81898
zafarali <zafarali.ahmed@gmail.com>
2018-03-22 22:11:21 -0400

    New death models for supplementary figures

--- model/compile_all_experiments.sh
+++ model/compile_all_experiments.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir experiments
 mkdir experiments/u0.01
+mkdir experiments/u0.01small
 mkdir experiments/u0.01875
 
-for mutation_rate in "u0.01" "u0.01875"; do
+for mutation_rate in "u0.01" "u0.01875" "u0.01small"; do
     for filename in $(echo "./parameter_files/$mutation_rate/*.h" | cut -d ' ' -f 1); do
         echo "compiling $filename";
         # mutarate=$(echo $filename | cut -d '/' -f 3);
