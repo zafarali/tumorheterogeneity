12e06b20c009601364d31704087566041fa12efb
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-03-29 18:28:09 -0400

    add observed

--- figures/create_fanning_plots_v2.py
+++ figures/create_fanning_plots_v2.py
@@ -11,6 +11,10 @@ sns.set_context('paper', font_scale=1.5)
 from figtools import *
 ALL_SEEDS = ['10','100','102','15','3','3318','33181','33185','33186','34201810','342018101','342018102','8','9','99']
 
+
+ALTERNATE_COLORS = sns.xkcd_palette(["vomit green", "orangish brown", "azure", "dark pink"])
+small_c, med_c, big_c, biggest_c = ALTERNATE_COLORS
+
 """
 Fanning Plots
 """
@@ -40,20 +44,28 @@ def plot_fanning_v2(root_folder, seeds, k=0.01, pts=100, cutoff='00', d_append='
 
 
     x,y = [No_turnover[(1,)][0], No_turnover[(1,)][1] + N*mu*0.05*(R_f(0.05) - Turnover005[(1,)][0])/(gamma * b) ]
+    ax.plot(Turnover005[(1,)][0], Turnover005[(1,)][1], '--', color=small_c, label='d=0.05, observed')
+    ax.plot(x, y, '-', color=small_c, label='d=0.05, predicted')
 
-    ax.plot(x, y, label='d=0.05')
+
     x,y = [No_turnover[(1,)][0], No_turnover[(1,)][1] + N*mu*0.1*(R_f(0.1) - Turnover01[(1,)][0])/(gamma * b) ]
+    ax.plot(Turnover01[(1,)][0], Turnover01[(1,)][1], '--', color=med_c, label='d=0.1, observed')
+    ax.plot(x, y, '-', color=med_c, label='d=0.1, predicted')
 
-    ax.plot(x, y, label='d=0.1')
+
+
     x,y = [No_turnover[(1,)][0], No_turnover[(1,)][1] + N*mu*0.2*(R_f(0.2) - Turnover02[(1,)][0])/(gamma * b) ]
+    ax.plot(Turnover02[(1,)][0], Turnover02[(1,)][1], '--', color=big_c, label='d=0.2, observed')
+    ax.plot(x, y, '-', color=big_c, label='d=0.2, predicted')
 
-    ax.plot(x, y, label='d=0.2')
+
+
     x,y = [No_turnover[(1,)][0], No_turnover[(1,)][1] + N*mu*0.65*(R_f(0.65) - Turnover065[(1,)][0])/(gamma * b) ]
-
-    ax.plot(x, y, label='d=0.65')
+    ax.plot(Turnover065[(1,)][0], Turnover065[(1,)][1], '--', color=biggest_c, label='d=0.65, observed')
+    ax.plot(x, y, '-', color=biggest_c, label='d=0.65')
 
     ax.set_xlabel('Distance from COM of Tumor')
-    ax.set_ylabel('Relative Increase in S(1)')
+    ax.set_ylabel('S(1)')
     ax.legend()
     ax.semilogy(basey=10)
     fig.tight_layout(h_pad=1)
