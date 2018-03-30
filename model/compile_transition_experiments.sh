0b9df0c969a3de5d341f9c3916b0dd7324f316b6
zafarali <zafarali.ahmed@gmail.com>
2018-03-30 16:17:48 -0400

    Add transition experiment compiler

--- model/compile_transition_experiments.sh
+++ model/compile_transition_experiments.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 mkdir experiments
-mkdir experiments/u0.01
-mkdir experiments/u0.01875
 mkdir experiments/u0.01transition
 
-for mutation_rate in "u0.01" "u0.01875" "u0.01transition"; do
+for mutation_rate in "u0.01transition"; do
     for filename in $(echo "./parameter_files/$mutation_rate/*.h" | cut -d ' ' -f 1); do
         echo "compiling $filename";
         # mutarate=$(echo $filename | cut -d '/' -f 3);
